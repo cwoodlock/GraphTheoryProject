9d41dcda80018ff577d09c6f852513abef9e3a24
cwoodlock <g00341460@gmit.ie>
2018-04-07 20:36:55 +0100

    Added files from video lectures

---
+++ shunt.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+func intopost(infix string) string {
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8} //Map to keep track of special chars, and order of precedence
+
+	pofix := []rune{}
+	s := []rune{} //s is stack
+
+	for _, r := range infix { //Range on string will convert it to array of runes
+		switch {
+		case r == '(':
+			s = append(s, r) //temp put open bracket on the end of the stack
+
+		case r == ')':
+			for s[len(s)-1] != '(' { //If we see a closin bracket we are going to pop things off the stack until we see a close bracket
+				pofix = append(pofix, s[len(s)-1]) //last element of s still need to pop it off
+				s = s[:len(s)-1]                   //everything up to last element and set it to s
+			}
+			s = s[:len(s)-1]
+
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1] //Condensed version of lines 20 + 21
+			}
+			s = append(s, r)
+
+		default:
+			pofix = append(pofix, r)
+		}
+	}
+
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
+
+	return string(pofix)
+}
+
+func main() {
+
+	//Answer ab.c*.
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ", intopost("a.b.c*"))
+
+	//Answer abd|.*
+	fmt.Println("Infix: ", "(a.(b|d))*")
+	fmt.Println("Postfix: ", intopost("(a.(b|d))*"))
+
+	//Answer abd|.c*
+	fmt.Println("Infix: ", "a.(b|d).c*")
+	fmt.Println("Postfix: ", intopost("a.(b|d).c*"))
+
+	//Answer abb.+.c.
+	fmt.Println("Infix: ", "a.(b.b)+.c")
+	fmt.Println("Postfix: ", intopost("a.(b.b)+.c"))
+}
